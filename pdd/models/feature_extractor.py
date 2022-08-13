1064d2e89106db28657ba37305eb029fc7772a2d
LesostepnoyGnom <73924501+LesostepnoyGnom@users.noreply.github.com>
2022-08-13 15:00:29 +0300

    Update feature_extractor.py

--- pdd/models/feature_extractor.py
+++ pdd/models/feature_extractor.py
@@ -38,7 +38,7 @@ def conv_block(n_filters,
 
 
 def get_feature_extractor(input_shape):
-    inputs = Input(shape=input_shape)
+    inputs = Input(input_shape)
     x = conv_block(32, (10, 10), batch_norm=True)(inputs)
     x = conv_block(64, (7, 7), batch_norm=True)(x)
     x = conv_block(128, (5, 5), batch_norm=True)(x)
